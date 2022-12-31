7b6b22e55e3905cea3f3927668f50f1c6ed9634e
noe <jesus.echeverria017@gmail.com>
2022-12-30 21:51:11 -0600

    Tailwind css instalado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,3 +3,15 @@
         SCHOOL BOOK
     @endslot
 @endcomponent
+@php
+        $items = [['route' => '/', 'text' => 'INICIO'],
+                  ['route' => '/', 'text' => 'CURSANDO'],
+                  ['route' => '/', 'text' => 'DOCUMENTOS'],
+                  ['route' => '/', 'text' => 'RUTA'],
+                  ['route' => '/', 'text' => 'CERRAR SESION']];
+        
+    @endphp
+    <x-menu :items="$items"></x-menu>
+    <h1 class="text-blue-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
