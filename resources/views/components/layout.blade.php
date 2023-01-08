33d58e9ad5e13d3442169f27407df3c14041d18d
noe <jesus.echeverria017@gmail.com>
2023-01-08 12:43:57 -0600

    Instalacion de animate.css (clases para animar componentes)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/main.scss','resources/js/app.js'])
-    
+    <link
+    rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <title>{{ $title }}</title>
 </head>
 <body>
